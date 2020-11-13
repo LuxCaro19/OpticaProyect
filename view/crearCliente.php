00333e6f6adb970f8930251b8c41c42aad364fdb
LuxCaro19 <luiscaroretamal2000@gmail.com>
2020-11-13 18:17:46 -0300

    Modelado avanzado de error de acceso por URL y Creacion de clientes

--- view/crearCliente.php
+++ view/crearCliente.php
@@ -8,6 +8,8 @@
     <title>Crear Cliente</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
 </head>
 <body>
 
@@ -16,62 +18,130 @@
     if(isset($_SESSION['user'])){?>
 
         <nav>
-            <div class="nav-wrapper">
-            <a href="#" class="brand-logo"><?=$_SESSION['user']['nombre']?></a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="sass.html">Crear Cliente</a></li>
-                <li><a href="cerrarSesion.php">Cerrar Sesión</a></li>
-            </ul>
+
+            <div class="nav-wrapper grey darken-1">
+
+
+                <img src="../img/logoOptica.png" alt="">
+        
+            
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="crearCliente.php">Crear Cliente</a></li>
+                    <li><a href="cerrarSesion.php">Cerrar Sesión</a></li>
+                </ul>
             </div>
         </nav>
 
-        <div class="container">
+            <div class="container">
                 
-                <div class="row">
+                <div class="row login">
 
                     <div class="col l4 m6 s12 offset-l4 offset-m3">
 
+                        <div class="card">
 
-                        <h1 class="center">Crear Cliente</h1>
+                            <div class="card-content">
 
-                        <div class="center">
-                            <p class="red-text">
-                                <?php
-                                    if (isset($_SESSION['error'])) {
-                                        echo $_SESSION['error'];
-                                        unset($_SESSION['error']);
-                                    }
-                                ?>
-                            </p>
+                                <h4 class="center">Crear Cliente</h4>
 
-                            <p class="green-text">
-                                <?php
-                                    if (isset($_SESSION['resp'])) {
-                                        echo $_SESSION['resp'];
-                                        unset($_SESSION['resp']);
-                                    }
-                                ?>
-                            </p>
+                                <div class="card-errors">
+
+                                    <p class="red-text center">
+                                        <?php
+                                            if (isset($_SESSION['error'])) {
+                                                echo $_SESSION['error'];
+                                                unset($_SESSION['error']);
+                                            }
+                                        ?>
+                                    </p>
+
+                                    <p class="green-text center">
+                                        <?php
+                                            if (isset($_SESSION['resp'])) {
+                                                echo $_SESSION['resp'];
+                                                unset($_SESSION['resp']);
+                                            }
+                                        ?>
+                                    </p>
+
+
+
+                                </div>
+
+                                <form action="../controllers/ControlCliente.php" method="POST">
+
+                                    <div class="input-field">
+
+                                        <input type="text" name="rut" id="rut">
+                                        <label for="rut">Rut</label>
+
+
+                                    </div>
+
+                                    <div class="input-field">
+
+                                        <input type="text" name="nombre" id="nombre">
+                                        <label for="nombre">Nombre</label>
+
+
+                                    </div>
+
+                                    <div class="input-field">
+
+                                        <input type="text" name="direccion" id="direccion">
+                                        <label for="direccion">Dirección</label>
+
+
+                                    </div>
+
+                                    <div class="input-field">
+
+                                        <input type="text" name="telefono" id="telefono">
+                                        <label for="telefono">Teléfono</label>
+
+
+                                    </div>
+
+                                    <div class="input-field">
+
+                                        <input type="text" class="datepicker" name="fecha" id="fecha">
+                                        <label for="fecha">Fecha</label>
+
+
+                                    </div>
+
+                                    <div class="input-field">
+
+                                        <input type="text" name="email" id="email">
+                                        <label for="email">Email</label>
+
+
+                                    </div>
+
+                                    <div class="input-field center-align">
+
+                                        <button class="btn-large">Iniciar Sesión</button>
+
+                                    </div>
+
+
+                                </form>
+
+
+
+                            </div>
 
                         </div>
+
                     
 
-                        <form action="../controllers/ControlCliente.php" method="POST">
+                    
 
-
-                            <input type="text" name="rut" placeholder="Rut">
-                            <input type="text" name="nombre" placeholder="Nombre">
-                            <input type="text" name="direccion" placeholder="Dirección">
-                            <input type="text" name="telefono" placeholder="Teléfono">
-                            <input type="text" name="fecha" placeholder="Fecha">
-                            <input type="text" name="email" placeholder="Email">
-
-                            <button class="btn ancho-100">Crear</button>            
+                                    
 
 
 
 
-                        </form>
 
 
 
@@ -93,19 +163,34 @@
 
     <?php }else{ ?>
 
-        <div class="container">
 
-            <div class="card-panel">
+        <div class="container center">
+                
+            <div class="row error">
 
-                <h1>Te has equivocado de camino amigo</h1>
-                <h3>no dispones de accesso para estar aquí</h3>
-                <p>debes iniciar sesión, vuelve al <a href="../index.php">home</a> e inicia sesión.</p>
+                <div class="col l6 m6 s12 offset-l3 offset-m3">
 
+                    <div class="card">
+
+                        <div class="card-content">
+
+                            <img src="../img/logoOptica.png" alt="">
+
+                            <h2 class="red-text">Te has equivocado de camino amigo</h2>
+                            <h4 class="black-text">no dispones de accesso para estar aquí</h4>
+                            <p>Debes iniciar sesión, vuelve al <a href="../index.php">home</a> e inicia sesión.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
 
-
         </div>
+
+
 
     <?php }?>
 
@@ -113,6 +198,27 @@
     
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.datepicker');
+            var instances = M.Datepicker.init(elems,{
+
+                'format': 'yyyy/mm/dd',
+                'i18n': {
+
+                    months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                    monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+                    weekday: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+                    weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+                    weekdaysAbbrev: ['D','L','M','M','J','V','S']
+
+                }
+
+            });
+        });
+
+    </script>
 
     
 </body>
